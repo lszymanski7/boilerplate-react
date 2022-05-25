c07bc491832e2e75c5ee8252498efc6d26df83c3
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-05-26 01:42:56 +0200

    Add new directories and dependencies

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
-console.log('Boilerplate React is running...')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<h1>Boilerplate React</h1>, document.getElementById('root')) 
