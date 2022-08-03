b1a17504b91fedab9eb698a79f5592614f7ad3a9
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-08-03 03:56:30 +0200

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
+import Boilerplate from './components/Boilerplate'
+import 'normalize.css/normalize.css'
+import './assets/styles/styles.scss'
 
-ReactDOM.render(<h1>Boilerplate React</h1>, document.getElementById('root')) 
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+	<Boilerplate />
+)
+
+console.log(
+    '+--------------+---------------------------------------------------------------------------------+' + '\n' +
+    '| Project name | React Boilerplate                                                               |' + '\n' +
+    '+--------------+---------------------------------------------------------------------------------+' + '\n' +
+    '| Description  | Boilerplate for creating projects in the JavaScript programming language based  |' + '\n' + 
+    '|              | on React and Redux libraries. It includes the basic configuration of tools such |' + '\n' + 
+    '|              | as Webpack, Babel, Jest, Express, ESLint and Prettier.                          |' + '\n' +
+    '+--------------+---------------------------------------------------------------------------------+' + '\n' +
+    '| Keywords     | Boilerplate, JavaScript, React, Redux, HTML, CSS, SASS, React Router, Webpack,  |' + '\n' + 
+    '|              | Babel, Jest, Testing Library, Node.js, Npm, Express, ESLint, Prettier           |' + '\n' +
+    '+--------------+---------------------------------------------------------------------------------+' + '\n' +
+    '| Version      | v1.0.0                                                                          |' + '\n' +
+    '+--------------+---------------------------------------------------------------------------------+'
+)
