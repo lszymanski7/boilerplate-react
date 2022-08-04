5474406482a15ad9d5c7b85f5cc203ed7b7b965a
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-08-04 03:07:22 +0200

    Update importFiles.js file

--- src/assets/importFiles.js
+++ src/assets/importFiles.js
@@ -1,3 +1,7 @@
-const logo = require('./images/logo256.png')
+const logo = {
+	basic128: require('./images/logo/logo128-basic.png'),
+	basic256: require('./images/logo/logo256-basic.png'),
+	basic512: require('./images/logo/logo512-basic.png')
+}
 
 export { logo as default }
