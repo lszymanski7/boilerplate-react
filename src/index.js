6b8befb195ac27b49c7513d26c34f1f98e419e5e
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-08-27 04:13:01 +0200

    Update index.html, server.js and index.js files

--- src/index.js
+++ src/index.js
@@ -2,29 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import Boilerplate from './components/Boilerplate'
-import store from './store/setupStore'
-import 'normalize.css/normalize.css'
-import './assets/styles/styles.scss'
+import store from './store'
+import '@csstools/normalize.css'
+import './assets/sass/main.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
     <Provider store={store}>
-	    <Boilerplate />
+        <Boilerplate />
     </Provider>
 )
 
 console.log(
-    '+--------------+---------------------------------------------------------------------------------+' + '\n' +
-    '| Project name | React Boilerplate                                                               |' + '\n' +
-    '+--------------+---------------------------------------------------------------------------------+' + '\n' +
-    '| Description  | Boilerplate for creating projects in the JavaScript programming language based  |' + '\n' + 
-    '|              | on React and Redux libraries. It includes the basic configuration of tools such |' + '\n' + 
-    '|              | as Webpack, Babel, Jest, Express, ESLint and Prettier.                          |' + '\n' +
-    '+--------------+---------------------------------------------------------------------------------+' + '\n' +
-    '| Keywords     | Boilerplate, JavaScript, React, Redux, HTML, CSS, SASS, React Router, Webpack,  |' + '\n' + 
-    '|              | Babel, Jest, Testing Library, Node.js, Npm, Express, ESLint, Prettier           |' + '\n' +
-    '+--------------+---------------------------------------------------------------------------------+' + '\n' +
-    '| Version      | v1.0.0                                                                          |' + '\n' +
+    '+--------------+---------------------------------------------------------------------------------+\n' +
+    '| Project name | React Boilerplate                                                               |\n' +
+    '+--------------+---------------------------------------------------------------------------------+\n' +
+    '| Description  | Boilerplate for creating projects in the JavaScript programming language based  |\n' +
+    '|              | on React and Redux libraries. It includes the basic configuration of tools such |\n' +
+    '|              | as Webpack, Babel, Jest, Express, ESLint and Prettier.                          |\n' +
+    '+--------------+---------------------------------------------------------------------------------+\n' +
+    '| Keywords     | Boilerplate, JavaScript, React, Redux, HTML, CSS, Sass, React Router, Webpack,  |\n' +
+    '|              | Babel, Jest, Testing Library, Node.js, Npm, Express, ESLint, Prettier           |\n' +
+    '+--------------+---------------------------------------------------------------------------------+\n' +
+    '| Version      | v1.0.0                                                                          |\n' +
     '+--------------+---------------------------------------------------------------------------------+'
 )
