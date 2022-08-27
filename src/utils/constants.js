ba528213900dfe8160676cdf6ba19ac9a41ce5bf
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-08-27 16:53:04 +0200

    Add utils directory

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+// React Logo
+
+const logo = {
+    size_128x128: require('../assets/images/logos/react-128x128.png'),
+    size_256x256: require('../assets/images/logos/react-256x256.png'),
+    size_512x512: require('../assets/images/logos/react-512x512.png')
+}
+
+export { logo as default }
---
+++ src/utils/file-transformer.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+
+const path = require('path')
+
+module.exports = {
+    process(sourceText, sourcePath, options) {
+        return {
+            code: `module.exports = ${JSON.stringify(path.basename(sourcePath))}`
+        }
+    }
+}
