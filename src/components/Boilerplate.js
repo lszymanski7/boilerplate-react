9f62292934375320848899079ef258f006aa91b6
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-08-27 16:54:20 +0200

    Update Boilerplate.js component

--- src/components/Boilerplate.js
+++ src/components/Boilerplate.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import logo from '../assets/importFiles'
+import logo from '../utils/constants'
 
 const Boilerplate = () => {
-	return (
-		<div className="container"> 
-		    <img className="container__logo" src={logo.basic256} alt="React Boilerplate" />
-		    <h1 className="container__title">React Boilerplate</h1>
-		</div>
-	)
+    return (
+        <div className="container">
+            <img
+                className="container__logo"
+                src={logo.size_256x256}
+                alt="React Boilerplate"
+            />
+            <h1 className="container__title">React Boilerplate</h1>
+        </div>
+    )
 }
 
 export { Boilerplate as default }
