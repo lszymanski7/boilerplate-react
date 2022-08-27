3f278bcaa194ee43839fc307080a3344081b3871
Łukasz Szymański <lukasz.szymanski96@gmail.com>
2022-08-27 16:45:17 +0200

    Update webpack.common.js, webpack.dev.js and webpack.prod.js files

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,38 +1,57 @@
 const common = require('./webpack.common.js')
 const { merge } = require('webpack-merge')
-const path = require('path')
 
 module.exports = merge(common, {
-	mode: 'development',
-	devtool: 'inline-source-map',
-	output: {
-		filename: 'index.bundle.js',
-		path: path.resolve(__dirname, '../dist')
-	},
-	module: {
-		rules: [
-			// CSS & SCSS
-			{
-				test: /\.(scss|css)$/,
-				use: [
-					'style-loader', 
-					'css-loader', 
-					'sass-loader'
-				]
-			}
-		]
-	},
-	devServer: {
-		static: {
-			directory: path.resolve(__dirname, '../dist')
-		},
-		client: {
-			logging: 'warn'
-		},
-		compress: true,
-		historyApiFallback: true,
-		hot: true,
-		open: true,
-		port: 8080
-	}
+    mode: 'development',
+    devtool: 'eval-source-map',
+    stats: 'minimal',
+    output: {
+        filename: 'static/js/[name].js',
+        chunkFilename: 'static/js/[name].chunk.js'
+    },
+    module: {
+        rules: [
+            // CSS | SCSS
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            // Fonts
+            {
+                test: /\.(woff(2)?|eot|ttf|otf)$/,
+                type: 'asset/inline',
+                generator: {
+                    filename: 'static/fonts/[name][ext]'
+                }
+            },
+            // Images
+            {
+                test: /\.(png|jpe?g|gif|svg|ico|webp|tiff)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'static/images/[name][ext]'
+                }
+            },
+            // Media
+            {
+                test: /\.(mp4|webm|wav|mp3|m4a|aac|oga)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'static/media/[name][ext]'
+                }
+            }
+        ]
+    },
+    devServer: {
+        client: {
+            logging: 'warn'
+        },
+        compress: true,
+        historyApiFallback: true,
+        hot: true,
+		liveReload: false,
+        open: true,
+        port: 8080,
+		static: false
+    }
 })
