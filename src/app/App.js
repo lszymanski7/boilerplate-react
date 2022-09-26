9b6bad67999234ec77ca1fc0fea51fdd34c75eb2
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2022-09-26 21:49:15 +0200

    Update App.js, Home.js, _flexbox.scss and _home.scss files

--- src/app/App.js
+++ src/app/App.js
@@ -5,7 +5,7 @@ import Home from '../pages/Home'
 
 const App = () => {
     return (
-        <div className="flexbox-fullscreen">
+        <div className="flexbox-column fullscreen">
             <Header />
             <Home />
             <Footer />
